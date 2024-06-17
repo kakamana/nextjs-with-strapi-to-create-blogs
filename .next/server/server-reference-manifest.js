15569089c46628647c6fad356acf0747fb2557ea
kakamana <kamig4u@gmail.com>
2024-06-17 23:26:58 +0400

    first check in for project creation
    first check in for project creation

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"arauWn9jEKF7XQJNCQ8MqqGeC8x2zFYHkYVxHkBfv28=\"\n}"
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>hello next js with strapi for blogging platform</h1>
+    </main>
+  );
+}
